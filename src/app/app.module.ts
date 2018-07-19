95d5cd3eb46f4765dc771307bf6443e32d37b35e
Alex Holanda <alex.holanda@outlook.com>
2018-07-18 23:17:43 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import LocalePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
+import { VendaCadastroComponent } from './venda-cadastro/venda-cadastro.component';
+
+registerLocaleData(LocalePt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    VendasListagemComponent,
+    VendaCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    CurrencyMaskModule,
+    TableModule,
+    DropdownModule,
+    InputTextModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
